5d2cc3992a60a0b648bd022ad3b991f9b0d047b5
alexm <alex-m@outlook.com>
2023-02-23 15:25:11 -0800

    Set up Flask app (config); create main block to connect to database and app

--- app.py
+++ app.py
@@ -8,3 +8,28 @@ Main Flask application.
 
 from flask import Flask, request, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db
+
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "O secreta foarte secreta"
+debug = DebugToolbarExtension(app)
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+
+app.config["SQALCHEMY_DATABASE_URI"] = "postresql:///adoption"
+
+
+
+
+# MAIN --------------------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+
+    connect_db(app)
+
+    with app.app_context():
+        db.create_all()
+
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=False)
