0410405b0ccd97a0f78ecd7c3a8903faa28a72d0
alexm <alex-m@outlook.com>
2023-02-23 17:13:34 -0800

    Create form class for the form to add a new pet

--- forms.py
+++ forms.py
@@ -7,3 +7,17 @@ Form model creation and setup.
 """
 
 from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, TextAreaField, SelectField
+
+
+class AddPetForm(FlaskForm):
+    """
+    Form for adding a new pet.
+    """
+
+    name = StringField("Pet name")
+    species = StringField("Species")
+    photo_url = StringField("Photo URL")
+    age = IntegerField("Age (years)")
+    notes = TextAreaField("Notes")
+    available = SelectField("Available (Y/N)")
