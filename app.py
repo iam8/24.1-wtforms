cf5d07d323519fb626022c5081211397c1b42905
alexm <alex-m@outlook.com>
2023-02-23 17:49:58 -0800

    Remove populate_obj code; populate new Pet object with form field values manually

--- app.py
+++ app.py
@@ -52,8 +52,13 @@ def add_pet():
     form = AddPetForm()
 
     if form.validate_on_submit():
-        new_pet = Pet()
-        form.populate_obj(new_pet)
+
+        name = form.name.data
+        species = form.species.data
+        age = form.age.data
+        notes = form.notes.data
+
+        new_pet = Pet(name=name, species=species, age=age, notes=notes)
         db.session.add(new_pet)
         db.session.commit()
         return redirect("/")
